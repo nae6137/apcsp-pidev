f93ae01270f888a0fd052661fd80bfdc58782257
nae6137 <emilyna99@gmail.com>
2018-05-24 18:29:31 +0000

    variables activity code

--- variables.c
+++ variables.c
@@ -5,12 +5,14 @@ int main()
 	int a;
 	int b =3;
 	int c;
+	int i;
 
 	a=2;
 	c = a + b;
-	printf("Sum of %d and %d is %d\n", a, b, c);
-	a = temp;
-	a =c ;
-	c= temp;
-printf
+
+for(i=0; i<2; i++);
+	a = i;
+	a = c;
+	c = i;
+	printf("a is %d and c is %d\n", a, c);
 }
