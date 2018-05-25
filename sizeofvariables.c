9ab2362894d236b2d97486b8db4898f0a9e229b9
nae6137 <emilyna99@gmail.com>
2018-05-25 18:51:01 +0000

    first commit; not working

---
+++ sizeofvariables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+  int a = 545;
+
+  // print value and size of an int variable
+  printf("int a value %d (oct %o, hex %x) and size %d bytes.\n", a, a, a, sizeo$
+}
+
+
