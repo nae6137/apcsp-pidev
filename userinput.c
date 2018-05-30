a9f4cead23e76cf3d3185441fbcc48babf8bc99d
nae6137 <emilyna99@gmail.com>
2018-05-30 20:16:44 +0000

    second commit, one thing right, more things wrong

--- userinput.c
+++ userinput.c
@@ -10,11 +10,15 @@ int main()
     {
     float b;
     float r;
-    char input [26];
+    char input[26];
     int x;
+    int* ptrtox;
+    int* ptrtoa;
+    ptrtoa = &x;
+
     printf("The circle's radius is %d\n");
     fgets (input, 26, stdin);
-    sscanf (input, %d\n", &x);
-    b = areaofcircle (x)
+    sscanf ("input, %d\n", &x);
+    b = areaofcircle (x);
     printf ("The circle's area is %f\n", b);
     }
